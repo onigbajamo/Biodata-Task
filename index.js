b4ff016575f2391c3dfc77661a8953302c64c485
onigbajamo <demiladeladipo25@gmail.com>
2022-06-22 13:48:29 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var firstName = "Demilade";
+var middleName = "S.";
+var lastName = "Ladipo";
+var height = "5ft6";
+var country = "Nigeria";
+
+name = firstName + " " + middleName + " " + lastName;
+
+document.write("Name:" + " " + name + "<br/>");
+document.write("Height:" + " " + height + "<br/>");
+document.write("Country:" + " " + country);
